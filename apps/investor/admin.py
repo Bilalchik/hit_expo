db39d7c9afb20e846fd30a4e054252da1429a054
user <panpanzov.nur@bk.ru>
2023-07-27 11:52:59 +0600

    fix git

--- apps/investor/admin.py
+++ apps/investor/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-from apps.investor.models import Investor, Country
-=======
 from apps.investor.models import Investor, Country , Organizer, Sponsors
->>>>>>> 5eb36137c99492841360ea843c19737cfdbd3369
 
 admin.site.register((Investor, Country,Organizer, Sponsors))
