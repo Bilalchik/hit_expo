0afcb2e1acf9edbd291a73a70cf6a7292e9f6139
user <panpanzov.nur@bk.ru>
2023-08-03 12:04:15 +0600

    add chat

--- apps/chat/serialzers.py
+++ apps/chat/serialzers.py
@@ -5,4 +5,7 @@ from apps.chat.models import Message
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = 'id', 'author', 'content', 'timestamp'
+        fields = 'id', 'author', 'content', 'timestamp', 'file'
+
+    def create(self, validated_data):
+        return super().create(validated_data)
--- main/asgi.py
+++ main/asgi.py
@@ -1,14 +1,6 @@
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from main.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': URLRouter(websocket_urlpatterns),
-})
+application = get_asgi_application()
