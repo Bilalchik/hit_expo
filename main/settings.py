e708f4a588134ff255c7936c17ca8158aac29f49
Bilal <azbk2004@gmail.com>
2023-09-04 12:02:54 +0600

    ,,,

--- main/settings.py
+++ main/settings.py
@@ -96,23 +96,23 @@ ASGI_APPLICATION = 'main.routing.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'expo_db',
-        'USER': 'hit_expo',
-        'PASSWORD': 'expo_123',
-        'HOST': 'pgdb',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'expo_db',
+#         'USER': 'hit_expo',
+#         'PASSWORD': 'expo_123',
+#         'HOST': 'pgdb',
+#         'PORT': '5432',
+#     }
+# }
 
 
 REST_FRAMEWORK = {
